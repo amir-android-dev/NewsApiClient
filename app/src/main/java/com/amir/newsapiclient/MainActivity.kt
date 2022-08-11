8facdccd06738834d667d04896a1deb84289e449
amir-android-dev <amir-hosseini@gmx.at>
2022-08-11 17:31:41 +0200

    recyclerview for newsFragment / creating adapter for NewsFragment.kt and implement it defining viewModel inside of MainActivity.kt and inherit inside of NewsFragment.kt

--- app/src/main/java/com/amir/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/amir/newsapiclient/MainActivity.kt
@@ -2,19 +2,35 @@ package com.amir.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.amir.newsapiclient.databinding.ActivityMainBinding
+import com.amir.newsapiclient.presentation.viewModel.NewsViewModel
+import com.amir.newsapiclient.presentation.viewModel.NewsViewModelFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+//To construct this view model, we need a view model factory. We are going to get it using dependency injection.
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    //for this project we use one activity and multiple fragment
+    //So, we will construct and instance of view model inside this main activity and share it among fragments. Let’s define a view model here.
+    @Inject
+    lateinit var factory: NewsViewModelFactory
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bnvNews.setupWithNavController(navController)
+
+        viewModel = ViewModelProvider(this,factory).get(NewsViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/amir/newsapiclient/presentation/adapter/NewsAdapter.kt
@@ -0,0 +1,70 @@
+package com.amir.newsapiclient.presentation.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.amir.newsapiclient.data.model.Article
+import com.amir.newsapiclient.databinding.NewsListItemBinding
+import com.bumptech.glide.Glide
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+//diffUtil
+    private val callback = object : DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            /*
+            DiffUtil uses this areItemsTheSame function to decide whether two objects represent the same Item in the old and new list.
+             We can use value of one property for this comparison. Here we take the url.
+             */
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            //Diffutil uses this areContentsTheSame function two decide whether two objects have the same data
+            return oldItem == newItem
+        }
+
+    }
+   //to get the AsyncListDiffer
+    val differ = AsyncListDiffer(this,callback)
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val binding = NewsListItemBinding
+            .inflate(LayoutInflater.from(parent.context),parent,false)
+        return NewsViewHolder(binding)
+    }
+
+
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.bind(article)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+//using viewBinding
+    inner class NewsViewHolder(private val binding:NewsListItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(article: Article){
+            Log.i("MYTAG","came here ${article.title}")
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            binding.tvPublishedAt.text = article.publishedAt
+            binding.tvSource.text = article.source.name
+
+            Glide.with(binding.ivArticleImage.context).
+            load(article.urlToImage).
+            into(binding.ivArticleImage)
+        }
+    }
+
+
+
+
+}
